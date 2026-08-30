856cbf42fede395084bb916b70a8332a2734fc82
ft-financetracker <admin.financetracker@gmail.com>
2026-08-30 14:10:07 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ft-pay-shell-v3';
+const CACHE_NAME='ft-pay-shell-v4';
 const APP_SHELL=[
   './','./index.html','./result.html','./offline.html','./manifest.webmanifest',
   './assets/ft-pay-mark.svg','./assets/ft-pay-logo.svg',
